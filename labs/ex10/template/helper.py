f3e3e48c54a440f61605250284c423a8a8e9b5d7
Daniil <daniil@likhobaba.ru>
2023-11-23 15:02:32 +0100

    ex10

--- labs/ex10/template/helper.py
+++ labs/ex10/template/helper.py
@@ -41,9 +41,16 @@ def build_distance_matrix(data, mu):
         data: numpy array of shape = (N, d). original data.
         mu:   numpy array of shape = (k, d). Each row corresponds to a cluster center.
     Returns:
-        squared distances matrix,  numpy array of shape (N, k):
-            row number i column j corresponds to the squared distance of datapoint i with cluster center j.
+        numpy array of shape (N, k):
+            squared distances matrix,
+            the value row i column j corresponds to the squared distance of datapoint i with cluster center j.
     """
     ####################################
     ### ___ Enter your code here ___ ###
     ####################################
+    distances = np.apply_along_axis(
+        lambda x: np.apply_along_axis(lambda y: np.linalg.norm(y - x), 1, mu),
+        1, 
+        data
+    )
+    return distances
